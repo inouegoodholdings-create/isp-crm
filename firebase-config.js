7dc3e6d9eb18f9539bbd5956c12d4c037132642a
inouegoodholdings-create <inoue.lifepartners@gmail.com>
2026-03-23 12:03:56 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyASRBgReW4vZ8lrcjVUL2unfuCadAj2HZs",
+  authDomain: "prism-desk.firebaseapp.com",
+  projectId: "prism-desk",
+  storageBucket: "prism-desk.firebasestorage.app",
+  messagingSenderId: "717774007532",
+  appId: "1:717774007532:web:03eedf3ec705c73a3409c9"
+};
+
+window.FIREBASE_CONFIG = firebaseConfig;
